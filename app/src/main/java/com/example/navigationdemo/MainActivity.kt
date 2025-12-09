024099af7775edbdfe1a36cea5c922b67a51eebc
qscrim <tochnonemaltsev@yandex.ru>
2025-12-09 12:17:40 +0700

    Работа со стеком возврата и NavDisplay

--- app/src/main/java/com/example/navigationdemo/MainActivity.kt
+++ app/src/main/java/com/example/navigationdemo/MainActivity.kt
@@ -12,7 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navigationdemo.ui.theme.NavigationDemoTheme
-
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import com.example.navigationdemo.screens.*
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavigationDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +34,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainScreen(modifier: Modifier = Modifier) {
+    val backStack = rememberNavBackStack(HomeScreen)
+    val onNavigation: (NavKey) -> Unit = {
+        backStack.add(it)
+    }
+    val onClearBackStack: () -> Unit = {
+        while (backStack.size > 1) {
+            backStack.removeLastOrNull()
+        }
+    }
+
+    NavDisplay(
+        backStack = backStack,
+        onBack = { backStack.removeLastOrNull() },
+        entryProvider = entryProvider {
+            entry<HomeScreen> {
+                Home(onNavigation)
+            }
+            entry<WelcomeScreen>(
+                metadata = mapOf("extraDataKey" to "extraDataValue")
+            ) { key ->
+                Welcome(onNavigation, key.name)
+            }
+            entry<ProfileScreen> {
+                Profile(onClearBackStack)
+            }
+        }
     )
 }
 
 @Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     NavigationDemoTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            MainScreen(modifier = Modifier.padding(innerPadding))
+        }
     }
 }
